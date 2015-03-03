b6b6416a651740d1687f0928bac053448a5af981
gmathers <gmathers@intriquip.com>
2015-03-03 09:29:49 -0600

    tweak kanban field order for Opportunities and combine both modules into iii-crm

--- iii-crm/__openerp__.py
+++ iii-crm/__openerp__.py
@@ -1,11 +1,12 @@
 {
-    'name' : 'Custom Filters and Groups on CRM Opportunities Kanban View',
+    'name' : 'Custom iii CRM modifications',
     'version' : '1.0.0',
     'author' : 'Garth Mathers',
     'category' : 'CRM',
     'website' : '',
     'description': """
         Custom Filters and Groups on CRM Opportunities Kanban View
+        Customize Field Order on CRM Opportunities Kanban View
     """,
     'depends' : ['base', 'sale_crm'],
     'data':[
