21f65d604c13b19e7390519e2ac09fd862471f18
gmathers <gmathers@intriquip.com>
2015-03-02 18:09:01 -0600

    add Opp Next Action filters and Province Group

---
+++ iii-crm-filter/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Custom Filters and Groups on CRM Opportunities Kanban View',
+    'version' : '1.0.0',
+    'author' : 'Garth Mathers',
+    'category' : 'CRM',
+    'website' : '',
+    'description': """
+        Custom Filters and Groups on CRM Opportunities Kanban View
+    """,
+    'depends' : ['base', 'crm'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
